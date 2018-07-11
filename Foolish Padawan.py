b7294d95f0a016d750994639b87aa4aaa0f468d6
Wtorian <40780436+Wtorian@users.noreply.github.com>
2018-07-10 19:40:42 -0500

    Add files via upload

---
+++ Foolish Padawan.py
@@ -0,0 +1,8 @@
+print ('Welcome young hero, what is thy name?')
+name = input()
+print  ('{name}, you have been hailed far and wide as a man of action')
+print ('I have a quest for the, will you accept the dangers involved?')
+accept = input()
+if "yes":
+	print ('your truly are brave'):
+	else ('child of darkness, begone with you')
